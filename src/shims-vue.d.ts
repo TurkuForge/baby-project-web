c5e73d50e39fa33f1ea3ba43292feac032c87f6f
Andreas Näsman <andreas.nasman@hibox.tv>
2021-04-07 23:18:40 +0300

    Fix linting issues

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any */
 declare module "*.vue" {
   import { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
