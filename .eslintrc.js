9bf42527c0835ab0c18840f1614570427c2a95c6
Andreas Näsman <andreas.nasman@hibox.tv>
2021-04-07 23:01:51 +0300

    Run Prettier on all applicable files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,27 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/vue3-recommended',
-    'standard',
-    'prettier'
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "standard",
+    "prettier",
   ],
   overrides: [
     {
       // List of file extensions to lint.
-      files: "**/*.{js,ts,vue}"
-    }
+      files: "**/*.{js,ts,vue}",
+    },
   ],
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+};
